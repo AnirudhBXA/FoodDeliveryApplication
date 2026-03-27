1a9f51297bcdfb6409209a050e0f8bacd2b147bb
Myakam Anirudh <myakam.anirudh@bounteous.com>
2026-03-27 11:35:55 +0530

    initial commit

---
+++ FoodDeliveyBackend/src/main/java/com/example/FoodDelivey/FoodDeliveyApplication.java
@@ -0,0 +1,13 @@
+package com.example.FoodDelivey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveyApplication.class, args);
+	}
+
+}
---
+++ FoodDeliveyBackend/src/test/java/com/example/FoodDelivey/FoodDeliveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FoodDelivey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
